97da5b63ce1753b184c1539a8cfb34d100e4733c
ThiruSivaMukilan <sivamukilanthiru07@gmail.com>
2026-03-21 16:00:59 +0530

    Updated

--- dataset_generate.py
+++ dataset_generate.py
@@ -1,19 +1,12 @@
 import pandas as pd
 import numpy as np
 import os
-
 np.random.seed(42)
-
 rows = 5000
 data = []
-
-# create folder if not exists
 os.makedirs("data", exist_ok=True)
-
 for i in range(rows):
-
     intern_id = f"RX{str(i+1).zfill(4)}"
-
     perf_type = np.random.choice(['high', 'medium', 'low'])
 
     if perf_type == 'high':
@@ -45,7 +38,6 @@ for i in range(rows):
         deadline_met = np.random.uniform(50, 75)
         attendance = np.random.uniform(60, 85)
         punctuality = np.random.choice([0, 1])
-
     else:
         sprints_total = np.random.randint(4, 8)
         sprints_done = np.random.randint(1, int(sprints_total*0.5))
@@ -60,31 +52,16 @@ for i in range(rows):
         deadline_met = np.random.uniform(20, 50)
         attendance = np.random.uniform(40, 60)
         punctuality = 0
-
-    # -----------------------------------
-    # 🔥 ADD NOISE (REALISM)
-    # -----------------------------------
     code_review_score += np.random.normal(0, 5)
     deadline_met += np.random.normal(0, 5)
     attendance += np.random.normal(0, 3)
-
-    # keep values in valid range
     code_review_score = np.clip(code_review_score, 0, 100)
     deadline_met = np.clip(deadline_met, 0, 100)
     attendance = np.clip(attendance, 0, 100)
-
-    # -----------------------------------
-    # 🔥 ADD MISSING VALUES (REAL-WORLD)
-    # -----------------------------------
     if np.random.rand() < 0.05:
         code_review_score = None
-
     if np.random.rand() < 0.05:
         attendance = None
-
-    # -----------------------------------
-    # STORE DATA
-    # -----------------------------------
     data.append([
         intern_id,
         sprints_done,
@@ -98,10 +75,6 @@ for i in range(rows):
         None if attendance is None else round(attendance, 2),
         punctuality
     ])
-
-# -----------------------------------
-# CREATE DATAFRAME
-# -----------------------------------
 columns = [
     "intern_id",
     "sprints_done",
@@ -115,13 +88,7 @@ columns = [
     "attendance_percentage",
     "punctuality"
 ]
-
 df = pd.DataFrame(data, columns=columns)
-
-# -----------------------------------
-# SAVE FILE
-# -----------------------------------
 df.to_csv("data/real_world_intern_data.csv", index=False)
-
 print("Real-world dataset created successfully!")
 print(df.head())
